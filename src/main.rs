f174b587fb5a1e91c5c28b7c57bee740284edb78
dlabaja <dlabaja.jan@seznam.cz>
2022-07-17 22:47:12 +0200

    wtf imagine using lazy_static dies from cringe

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::fs::File;
 use std::*;
 use std::path::Path;
-use rgb::RGB8;
-use ansi_rgb::{Foreground, WithForeground};
 use image::codecs::gif::GifDecoder;
 use image;
+use eventual::Timer;
 
-use image::AnimationDecoder;
+use image::{AnimationDecoder, Frame};
 
 const PATH: &str = "/home/dlabaja/Downloads/bad_apple.gif";
 
@@ -23,24 +22,34 @@ fn main() {
     let file = File::open(Path::new(PATH));
 
     //decode to frames
-    let frames = GifDecoder::new(file.unwrap()).unwrap().into_frames();
+    let frames = GifDecoder::new(file.unwrap()).unwrap().into_frames().collect_frames().unwrap().to_vec();
+
+    //timer
+    let timer = Timer::new();
+    let ticks = timer.interval_ms(40).iter();
 
     //iterate frames
     let mut i = 0;
-    for frame in frames {
+    let max_frames = frames.len();
+    for _ in ticks {
+        if i == max_frames - 1 { break; }
+        process_frame(frames.get(i).unwrap(), i);
         i += 1;
-        let frame = frame.unwrap();
-        let mut pixels: String = "".to_string();
-        for line in frame.buffer().chunks(frame.buffer().width() as usize * 4) {
-            for pixel in line.chunks(4) {
-                pixels += &*format!("\x1b[38;2;{};{};{}m██", pixel[0], pixel[1], pixel[2]);
-            }
-            pixels += "\n";
-        }
-        print!("{}[2J", 27 as char);
-        println!("{}\n{}",pixels, i);
-        thread::sleep(time::Duration::from_millis(40));
     }
+    println!("Konec");
+
+}
+
+fn process_frame(frame: &Frame, index: usize) {
+    let mut pixels: String = "".to_string();
+    for line in frame.buffer().chunks(frame.buffer().width() as usize * 4) {
+        for pixel in line.chunks(4) {
+            pixels += &*format!("\x1b[38;2;{};{};{}m██", pixel[0], pixel[1], pixel[2]);
+        }
+        pixels += "\n";
+    }
+    print!("{}[2J", 27 as char);
+    println!("{}\n{}", pixels, index);
 }
 
 fn get_path() -> String {
