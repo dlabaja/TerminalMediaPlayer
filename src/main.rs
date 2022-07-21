7d0330c2e5d1a4bf94f03771b8a071ffa93801eb
dlabaja <dlabaja.jan@seznam.cz>
2022-07-21 11:49:24 +0200

    caching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::{DirBuilder, File};
 use std::*;
 use std::path::Path;
 use image::codecs::gif::GifDecoder;
@@ -11,26 +11,35 @@ const FPS: usize = 20;
 const VIDEO_FORMATS: [&str; 9] = ["mp4", "m4v", "mkv", "webm", "mov", "avi", "wmv", "mpg", "flw"];
 
 fn main() {
+    //TODO pauza tlačítko, max terminal
+
     //check ffmpeg
     Command::new("ffmpeg").output().expect("FFMPEG NOT FOUND! Please install one at https://ffmpeg.org/");
 
     //open file
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {panic!("Expected 1 argument, got {}! Hint - add filepath as the argument", args.len() - 1)}
+    if args.len() != 2 { panic!("Expected 1 argument, got {}! Hint - add filepath as the argument", args.len() - 1) }
     let path = Path::new(args[1].trim());
-    if File::open(path).is_err() || !is_video(path) {panic!("Invalid path or unsupported file!")}
+    if File::open(path).is_err() || !is_video(path) { panic!("Invalid path or unsupported file!") }
 
     println!("Path is right - processing video (might take some time)");
 
+    //upsert cache folder
+    let cache_folder = &format!("{}{}TerminalMediaPlayer", dirs::video_dir().unwrap().display(), get_system_backslash());
+    if File::open(cache_folder).is_err() {
+        DirBuilder::new().create(cache_folder).expect(&*format!("Unable to create cache folder in {}", dirs::video_dir().unwrap().display()));
+    }
+
     //convert video
     println!("Converting video");
-    let video = &format!("{}{}output.gif", dirs::data_local_dir().unwrap().display(), get_system_backslash());
-    Command::new("ffmpeg").args(["-i", &path.display().to_string(), "-vf", &format!("scale={}:{},fps=20", term_size::dimensions().unwrap().1.clamp(16, 96), term_size::dimensions().unwrap().0.clamp(9, 54)), &format!("{}", Path::new(video).display()), "-y"]).output().expect("Unable to convert to gif");
+    let video = &format!("{}{}{}.gif", cache_folder, get_system_backslash(), path.file_stem().unwrap().to_str().unwrap());
+    ffmpeg_handler(vec!["-vf", &format!("scale={}:{},fps=20", term_size::dimensions().unwrap().1.clamp(16, 96), term_size::dimensions().unwrap().0.clamp(9, 54))],
+                   path.to_str().unwrap(), video);
 
     //convert audio
     println!("Converting audio");
-    let audio = &format!("{}{}output.mp3", dirs::data_local_dir().unwrap().display(), get_system_backslash());
-    Command::new("ffmpeg").args(["-i", &path.display().to_string(), &format!("{}", Path::new(audio).display()), "-y"]).output().expect("Unable to convert audio");
+    let audio = &format!("{}{}{}.mp3", cache_folder, get_system_backslash(), path.file_stem().unwrap().to_str().unwrap());
+    ffmpeg_handler(vec![], path.to_str().unwrap(), audio);
 
     //decode to frames
     println!("Processing frames");
@@ -53,6 +62,21 @@ fn main() {
         process_frame(frames.get(i).unwrap(), i);
     }
     println!("End of playback\nhttps://github.com/dlabaja/TerminalMediaPlayer");
+}
+
+fn ffmpeg_handler(ffmpeg_args: Vec<&str>, input_path: &str, output_path: &str){
+    if File::open(output_path).is_err() {
+        let mut args = vec!["-i", input_path];
+        for arg in ffmpeg_args{
+            args.push(arg);
+        }
+        args.append(&mut vec![output_path, "-y"]);
+
+        Command::new("ffmpeg").args(args)
+            .output().unwrap_or_else(|_| panic!("Ffmpeg can't convert the video from {} to {}", input_path, output_path));
+        return;
+    }
+    println!("Video found in cache ({}), aborting conversion", output_path)
 }
 
 fn is_video(path: &Path) -> bool {
@@ -82,12 +106,6 @@ fn process_frame(frame: &Frame, index: usize) {
     }
     print!("{}[2J", 27 as char);
     println!("{}\x1b[38;2;255;255;255mframe={}/time={}s", pixels, index, secs_to_secs_and_mins(index / FPS));
-}
-
-fn get_path() -> String {
-    let mut input = "".to_string();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().to_string()
 }
 
 fn secs_to_secs_and_mins(secs: usize) -> String {
